e372e4224a04b19fe3b85e0407658644aa69536d
mikwitz <mikaylahowitz@gmail.com>
2022-03-13 14:10:53 -0600

    gallery with photos changing w nav links

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ const [currentCategory, setCurrentCategory] = useState(categories[0]);
     currentCategory={currentCategory}
     ></Nav>
       <main>
-      <Gallery></Gallery>
+      <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
       </main>
     </div>
